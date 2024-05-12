a5f5bf90db70b5da0fcabf483b6a1203a1d4c3ec
Seanomad61 <161769498+Nomad1961@users.noreply.github.com>
2024-05-12 13:13:27 +0300

    Summary

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,6 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { Toast } from 'izitoast';
+
 import 'izitoast/dist/css/iziToast.min.css';
 
 const renderImages = async images => {
